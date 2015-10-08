c0e8ce318d3abb308d51b73a2d4e2a2ded7a615c
Jon Harrison <jonjam1990@googlemail.com>
2015-10-08 20:58:23 +0100

    Creating loading indicator and hooked up.

---
+++ Assets/Scripts/LoadingIndicator.cs
@@ -0,0 +1,53 @@
+﻿namespace MarvelUniverse
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// The loading indicator.
+    /// </summary>
+    public class LoadingIndicator : MonoBehaviour
+    {
+        /// <summary>
+        /// The is loading animator parameter name.
+        /// </summary>
+        private const string IsLoadingAnimatorParameterName = "IsLoading";
+
+        /// <summary>
+        /// The animator.
+        /// </summary>
+        private Animator animator;
+
+        /// <summary>
+        /// The is loading animator parameter identifier.
+        /// </summary>
+        private int isLoadingAnimatorParameterId;
+
+        /// <summary>
+        /// The list of stone mesh renderers.
+        /// </summary>
+        public MeshRenderer[] stones;
+
+        /// <summary>
+        /// Handles the awake event.
+        /// </summary>
+        private void Awake()
+        {
+            this.animator = this.GetComponent<Animator>();
+            this.isLoadingAnimatorParameterId = Animator.StringToHash(LoadingIndicator.IsLoadingAnimatorParameterName);
+        }
+
+        /// <summary>
+        /// Handles the loading event.
+        /// </summary>
+        /// <param name="isLoading">A value indicating whether is loading.</param>
+        private void HandleLoading(bool isLoading)
+        {
+            foreach (MeshRenderer meshRenderer in this.stones)
+            {
+                meshRenderer.enabled = isLoading;
+            }
+
+            this.animator.SetBool(this.isLoadingAnimatorParameterId, isLoading);
+        }
+    }
+}
