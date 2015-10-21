43a17f5ef44532b2070dae2f754505317d1ca39c
Jon Harrison <jonjam1990@googlemail.com>
2015-10-21 08:05:14 +0100

    Working MessageDialog

---
+++ Assets/Scripts/TestModalWindow.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class TestModalWindow : MonoBehaviour
+{
+    private ModalPanel modalPanel;
+
+    private DisplayManager displayManager;
+
+
+    private void Awake()
+    {
+        this.modalPanel = ModalPanel.Instance();
+        this.displayManager = DisplayManager.Instance();
+    }
+
+    public void TestYNC()
+    {
+        ModalPanelDetails panelDetails = new ModalPanelDetails()
+        {
+            Title = "This is the title",
+            Message = "This is an announcement",
+            Button1Details = new EventButtonDetails()
+            {
+                ButtonTitle = "Yes",
+                Action = this.TestYesFunction
+            },
+            Button2Details = new EventButtonDetails()
+            {
+                ButtonTitle = "No",
+                Action = this.TestNoFunction
+            },
+            Button3Details = new EventButtonDetails()
+            {
+                ButtonTitle = "Cancel",
+                Action = this.TestCancelFunction
+            }
+        };
+
+        this.modalPanel.NewChoice(panelDetails);
+    }
+
+
+    private void TestYesFunction()
+    {
+        this.displayManager.DisplayMessage("Heck yeah! Yup!");
+    }
+
+    private void TestNoFunction()
+    {
+        this.displayManager.DisplayMessage("Heck no! No!");
+    }
+
+    private void TestCancelFunction()
+    {
+        this.displayManager.DisplayMessage("Cancelled.");
+    }
+}
