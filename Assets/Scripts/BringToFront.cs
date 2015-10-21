7184784e884b567a543c001b50e5621676aa8eed
Jon Harrison <jonjam1990@googlemail.com>
2015-10-21 08:05:14 +0100

    Working MessageDialog

---
+++ Assets/Scripts/BringToFront.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BringToFront : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        this.transform.SetAsLastSibling();
+    }
+}
